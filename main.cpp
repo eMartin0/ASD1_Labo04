eb10d86a8eb5e261e79056634f3b4d4c0b0cb7b7
eMartin0 <eloise.martin@me.com>
2018-05-13 11:33:18 +0200

    update

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ ostream& operator<<(ostream& os, const LinkedList<T>& liste);
 
 template < typename T > class LinkedList {
    friend ostream& operator<< <T>(ostream& os, const LinkedList<T>& liste);
-
+   //friend class Int;
 public:
    using value_type = T;
    using reference = T&;
@@ -62,8 +62,8 @@ private:
     *  @brief Nombre d'éléments
     */
    size_t nbElements;
-
-public:
+   
+   public:
 
    /**
     *  @brief Constructeur par défaut. Construit une LinkedList vide
@@ -79,11 +79,8 @@ public:
     *
     *  @param other la LinkedList à copier
     */
-   LinkedList(LinkedList& other) {
-      if (head != other.head) {
-         head = other.head;
-         nbElements = other.nbElements;
-      }
+   LinkedList(LinkedList& other) : nbElements(0), head(nullptr) {
+      *this = other;
    }
 
 public:
@@ -102,10 +99,31 @@ public:
     *  effacé.
     */
    LinkedList& operator=(const LinkedList& other) {
-      if (head->data != other.head->data && head->next != other.head->next) {
-         head->data = other.head->data;
-         head->next = other.head->next;
-         nbElements = other.nbElements;
+      if (this != &other) {
+         Node* currElement = head;
+         size_t nbToDel = nbElements;
+         size_t exc_safe = 0;
+
+         if (other.size() > 0) {
+            try {
+               insert(other.head->data, nbElements);
+               currElement = other.head->next;
+               exc_safe++;
+
+               while (currElement != nullptr) {
+                  insert(currElement->data, nbElements);
+                  currElement = currElement->next;
+                  exc_safe++;
+               }
+            } catch (std::logic_error& e) {
+               del(exc_safe);
+               throw;
+            }
+         }
+
+         for (size_t i = 0; i < nbToDel; i++) {
+            pop_front();
+         }
       }
       return *this;
    }
@@ -288,6 +306,15 @@ public:
          --nbElements;
       }
    }
+   
+private:
+      
+   void del(size_t pos) {
+      while (pos) {
+         erase(nbElements - pos);
+         pos--;
+      }
+   }
 
 public:
 
@@ -301,46 +328,67 @@ public:
        n'est pas trouvée
     */
    size_t find(const_reference value) const noexcept {
-      size_t pos = 1;
+      size_t pos = 0;
       Node* currElement = head;
-      while (currElement->data != value) {
-         pos++;
+
+      for (int i = 0; i < nbElements; ++i) {
+         if (currElement->data == value) {
+            return pos;
+
+         } else {
+            pos++;
+         }
          currElement = currElement->next;
       }
-      return pos ? pos : -1;
+
+      return pos = -1;
    }
 
    /**
     *  @brief Tri des elements de la liste par tri fusion
     */
-   void mergeSort(reference List, size_t first, size_t last);
-   void merge(reference list, size_t first, size_t middle, size_t last);
+
+   void swapElements(Node*& a, Node*& b) {
+      swap(a, b);
+      swap(a->next, b->next);
+   }
+
+   Node*& merge(Node*& start, Node* middle, Node* end) {
+      size_t pos = 0;
+      while (start != middle) {
+         if (start->data < middle->data) {
+            swapElements(middle, start);
+            start = start->next;
+            pos++;
+         } else {
+            swapElements(start, middle);
+            middle = middle->next;
+            pos++;
+         }
+
+      }
+      return start;
+   }
+
+   Node*& mergeSort(Node*& start, size_t nbElement) {
+      if (nbElement > 0) {
+         size_t middle = nbElements / 2;
+         Node *half = head, *full = head;
+         while (full && full->next) {
+            half = half->next;
+            full = full->next->next;
+         }
+
+         mergeSort(start, middle);
+         mergeSort(half, nbElement);
+         merge(start, half, full);
+      }
+      return start;
+
+   }
 
    void sort() {
-      mergeSort(head, 1, nbElements);
-   }
-
-   void mergeSort(reference list, size_t first, size_t last) {
-      if (last <= first) {
-         return;
-      }
-      /*Node* half = head;
-      Node full = head;
-      while(full && full->next){
-         half = half->next;
-         full = full->next->next;
-      }
-      delete full;*/
-      size_t middle = first + (last - first) / 2;
-
-      mergeSort(list, first, middle);
-      mergeSort(list, middle + 1, last);
-
-      merge(list, first, middle, last);
-   }
-
-   void merge(reference list, size_t first, size_t middle, size_t last) {
-      
+      mergeSort(head, nbElements);
    }
 };
 
@@ -356,46 +404,214 @@ ostream& operator<<(ostream& os, const LinkedList<T>& liste) {
    return os;
 }
 
-int main() {
+class Int {
+   int val;
+public:
 
-   const int N = 10;
+   Int() : val(0) {
+   }
 
-   cout << "Creation d'une liste de " << N << " entiers aleatoires \n";
-   LinkedList<int> liste;
+   Int(int i) : val(i) {
+      if (i == -1) throw std::logic_error("Int::Constructor");
+   }
+
+   Int(const Int& i) : val(i.val) {
+      if (i.val == -2) throw std::logic_error("Int::CopyConstructor");
+   }
+
+   Int& operator=(const Int& i) {
+      if (i.val == -3) throw std::logic_error("Int::Operator=");
+      val = i.val;
+      cout << "(I=" << val << ") ";
+      return *this;
+   }
+
+   bool operator<(const Int& i) {
+      return val < i.val;
+   }
+
+   bool operator==(const Int& i) {
+      return val == i.val;
+   }
+
+   friend ostream& operator<<(ostream& os, const Int& i);
+};
+
+ostream& operator<<(ostream& os, const Int& i) {
+   os << i.val;
+   return os;
+}
+
+using T = Int;
+
+int main(int argc, const char * argv[]) {
+
+   const int N = 9;
+
+   cout << "Creation d'une liste de " << N << " entiers aléatoires \n";
+   LinkedList<T> liste;
    for (unsigned i = 0; i < N; ++i) {
       liste.push_front(rand() % 100);
    }
    cout << "\n" << liste;
 
-   cout << "\nModification de l'element en tete a 42 \n";
+   cout << "\nModification de l'élément en tête à 42";
    liste.front() = 42;
    cout << "\n" << liste;
 
-   cout << "\nModification de l'element en position " << N / 2 << " a 24 \n";
-   liste.at(N / 2) = 24;
+   cout << "\nModification de l'élément en position " << N / 2 << " à 43";
+   liste.at(N / 2) = 43;
    cout << "\n" << liste;
 
-   cout << "\nSuppression de l'element en position " << 2 * N / 3 << "\n";
-   liste.erase(2 * N / 3);
+   cout << "\nSuppression de l'élément en position " << N / 3 << "\n";
+   liste.erase(N / 3);
    cout << "\n" << liste;
 
-   cout << "\nInsertion de l'element 421 en position 0\n";
-   liste.insert(421, 0);
+   cout << "\nSuppression de l'élément en dernière position \n";
+   liste.erase(liste.size() - 1);
    cout << "\n" << liste;
 
-   cout << "\nInsertion de l'element 422 en position " << N / 3 << "\n";
-   liste.insert(422, N / 3);
+   cout << "\nInsertion de l'élément 44 en position 0\n";
+   liste.insert(44, 0);
    cout << "\n" << liste;
 
-   cout << "\nInsertion de l'element 423 en derniere position\n";
-   liste.insert(423, liste.size());
+   cout << "\nInsertion de l'élément 45 en position " << N / 3 << "\n";
+   liste.insert(45, N / 3);
    cout << "\n" << liste;
 
-   cout << "\nCherche valeur 64 et retourne la position : ";
-   cout << liste.find(423);
-   cout << "\n" << liste << "\n";
+   cout << "\nInsertion de l'élément 46 en dernière position\n";
+   liste.insert(46, liste.size());
+   cout << "\n" << liste;
 
-   cout << "\nDestruction de la liste \n";
+   cout << "\n\nRecherche d'elements allant de 0 à 99 \n";
+   for (int i = 0; i < 100; ++i) {
+      if (liste.find(i) != -1)
+         cout << '(' << i << ')';
+   }
+
+   {
+      cout << "\n\nCreation d'une copie constante de la liste\n";
+      const LinkedList<T> copie = liste;
+      cout << "\n" << copie.size() << ": ";
+      for (size_t i = 0; i < copie.size(); ++i)
+         cout << copie.at(i) << " ";
+      cout << "\nDestruction de la copie constante\n";
+   }
+
+   {
+      cout << "\n\nCreation d'une copie de la liste\n";
+      LinkedList<T> copie = liste;
+      cout << "\nCopie - " << copie;
+
+      cout << "\nSuppression des " << N / 2 << " premiers éléments\n";
+      for (int i = 0; i < N / 2; ++i)
+         copie.pop_front();
+      cout << "\nCopie - " << copie;
+
+      cout << "\nRecopie de la liste original dans la copie\n";
+      copie = liste;
+
+      cout << "\nCopie - " << copie;
+      cout << "\nDestruction de la copie\n";
+   }
+
+   try {
+      cout << "\n\nInsertion hors de range";
+      liste.insert(42, liste.size() + 1);
+      cout << "\nException non levée";
+   } catch (std::out_of_range& e) {
+      cout << "\nstd::out_of_range capturée ";
+   } catch (...) {
+      cout << "\nPas la bonne exception";
+   }
+
+   try {
+      cout << "\nSuppression hors de range";
+      liste.erase(liste.size());
+      cout << "\nException non levée";
+   } catch (std::out_of_range& e) {
+      cout << "\nstd::out_of_range capturée ";
+   } catch (...) {
+      cout << "\nPas la bonne exception";
+   }
+
+   try {
+      cout << "\nAccès hors de range";
+      liste.at(liste.size()) = 42;
+      cout << "\nException non levée";
+   } catch (std::out_of_range& e) {
+      cout << "\nstd::out_of_range capturée ";
+   } catch (...) {
+      cout << "\nPas la bonne exception";
+   }
+
+   {
+      cout << "\nCreation d'une liste vide";
+      LinkedList<double> listeVide;
+      try {
+         cout << "\nAccès à l'élément de tête";
+         listeVide.front() = 1;
+         cout << "\nException non levée";
+      } catch (std::runtime_error& e) {
+         cout << "\nstd::runtime_error capturée ";
+      } catch (...) {
+         cout << "\nPas la bonne exception";
+      }
+
+      try {
+         cout << "\nSuppression d'un élément";
+         listeVide.pop_front();
+         cout << "\nException non levée";
+      } catch (std::runtime_error& e) {
+         cout << "\nstd::runtime_error capturée ";
+      } catch (...) {
+         cout << "\nPas la bonne exception";
+      }
+   }
+
+   try {
+      cout << "\n\nInsertion avec exception à la copie";
+      liste.push_front(-2);
+   } catch (...) {
+      cout << "\nException capturée";
+      cout << "\n" << liste;
+   }
+
+   try {
+      cout << "\n\nMise à -2 d'un élément de la liste (entraine une exception si il est copié) \n";
+      liste.at(liste.size() / 2) = -2;
+      cout << "\nCopie de liste avec exception à la copie d'un élément\n";
+      LinkedList<T> copie = liste;
+      cout << "\nException non levée ";
+   } catch (...) {
+      cout << "\nException capturée \n";
+   }
+
+   {
+      cout << "\n\nPréparation d'une liste temporaire\n";
+      LinkedList<T> copie;
+      for (int i = 0; i < 5; ++i)
+         copie.push_front(i);
+      cout << "\n" << copie << "\n";
+
+      try {
+         cout << "\nAffectation de liste avec exception à la copie d'un élément\n";
+         copie = liste;
+         cout << "\nException non levée";
+      } catch (...) {
+         cout << "\nException capturée";
+         cout << "\n" << copie << "\n";
+         liste.at(liste.size() / 2) = 123;
+      }
+      cout << "\nDestruction de la liste temporaire\n";
+   }
+
+   cout << "\n\nTri fusion";
+   cout << "\nAvant: " << liste;
+   liste.sort();
+   cout << "\nAprès: " << liste;
+
+   cout << "\nDestruction liste \n";
 
    return EXIT_SUCCESS;
 }
